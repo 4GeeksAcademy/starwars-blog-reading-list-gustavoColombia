4aada5e011aa9117cf5e92f1716824a966d24360
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-06-19 16:27:13 +0000

    block-04

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -7,8 +7,32 @@ export const Card = () => {
     <>
       {store.people.map((person) => {
         return (
-          
-            <p>{person.name}</p>
+            
+            <div className="col-3" key= {person.id}>
+              <h2>
+                 <span className="badge text-bg-secondary">Personajes</span>
+              </h2>
+              <div className="card mt-4">
+                <div class="card-body">
+                 <h5 class="card-title">{person.name}</h5>
+                  <p class="card-text">
+                    <img
+                      src={`https://starwars-visualguide.com/assets/img/characters/${
+                        person.url.split("/")[5]
+                       }.jpg`}
+                      alt="aqui esta la imagen"
+                      width="300px"
+                    />
+                    <div className="p-3">gander:{person.gender}</div>
+             <div className="p-3"> hair_color: {person.hair_color}</div>
+                 <div className="p-3">color: {person.eye_color}</div>
+                  </p>
+                  <button className="btn btn-outline-primary btn-lg d-flex justify-content-start">
+                     Learn more!
+                   </button>
+               </div>
+              </div>
+             </div>
           
         );
       })}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../styles/home.css";
-import Card from "../component/card.jsx";
+import {Card} from "../component/card.jsx";
 
 export const Home = () => {
   return (
-    <div className="row">
+    <div className="container-fluid">
+    <div className="row overflow-auto">
     <Card/>
+    </div>
     </div>
   );
 };
